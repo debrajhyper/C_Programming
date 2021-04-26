4136c22fbed9f06c0ffcf34a9ba0b7960fda21e8
Debraj Karmakar <64654268+debrajhyper@users.noreply.github.com>
2021-04-26 12:10:58 +0530

    Add files via upload

---
+++ sizeof Operators in C.c
@@ -0,0 +1,15 @@
+#include<stdio.h>>
+int main()
+{
+	int a;
+	char b;
+	long c;
+	double d;
+	printf("size of int=%d\n",sizeof(a));
+	printf("size of char=%d\n",sizeof(b));
+	printf("size of long=%d\n",sizeof(c));
+	printf("size of double=%d\n",sizeof(d));
+	printf("size of 175=%d\n",sizeof(175));
+
+	return 0;
+}
